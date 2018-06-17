58ec0cf999c0ed5533b22c61317c5d2449e100c3
Joaquín Herrera <herrerab.joaquin@gmail.com>
2018-06-17 15:23:12 +0000

    automatically backed up by learn

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -6,4 +6,30 @@ class Triangle
     @b = b
     @c = c
   end
+
+  def kind
+    if @a <= 0 || @b <=0 || @c <= 0 || @a + @b <= @c || @a + @c <= @b || @b + @c <= @a
+      begin
+        raise TriangleError
+      rescue TriangleError => error
+        puts error.message
+      end    
+    elsif @a == @b && @a == @c
+      :equilateral
+    elsif @a == @b || @a == @c || @b == @c
+      :isosceles
+    else
+      :scalene
+
+
+    end
+  end
+
+  class TriangleError < StandardError
+    def message
+      "check numbers"
+    end
+  end
+  
 end
+
